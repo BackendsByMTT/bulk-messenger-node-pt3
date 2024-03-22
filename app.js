6873d71682c4d533df1980eabb9cd9fd0baad45f
TrippyGaurav <gaurav.trippybug@gmail.com>
2024-03-22 15:34:07 +0530

    test

--- app.js
+++ app.js
@@ -1,33 +1,85 @@
 const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
 const healthCheckRoute = require("./src/routes/healthRoute");
 const messengerRoute = require("./src/routes/messengerRoute");
 const extensionRoute = require("./src/routes/extensionRoute");
 
 require("dotenv").config();
 const cors = require("cors");
-const wss = require("./src/utlis/socket");
+const {
+  addTask,
+  getPendingTask,
+  updateTaskStatus,
+  generateUniqueId,
+} = require("./src/utlis/actions");
 
 const app = express();
+const server = http.createServer(app); // Create an HTTP server
+const wss = new WebSocket.Server({ server }); // Create a WebSocket server
 const PORT = process.env.PORT || 3001;
+let taskSchedulerIntervalId = null;
 
 app.use(cors());
 app.use(express.json());
 
-wss.on("listening", () => {
-  console.log("WebSocket server listening on port 8080");
-});
 app.use("/api/", messengerRoute);
 app.use("/", healthCheckRoute);
 app.use("/extension", extensionRoute);
 
+wss.on("connection", (ws) => {
+  console.log(`WebSocket : ${wss.address.port}`);
+  ws.send(JSON.stringify({ action: "connected" }));
+
+  ws.on("message", async (message) => {
+    try {
+      const data = JSON.parse(message);
+      if (data.action === "addTask") {
+        await processAddTasks(ws, data.payload);
+      }
+    } catch (error) {
+      console.error("Error processing message:", error);
+    }
+  });
+
+  ws.on("close", () => {
+    console.log(`WebSocket connection closed`);
+  });
+});
+
+const processAddTasks = async (ws, payload) => {
+  const { message, ids: users, time: interval, count } = payload;
+
+  console.log("payload : ", payload);
+
+  await Promise.all(users.map((user) => addTask(user, message)));
+
+  scheduleTasks(ws, interval, count);
+};
+
+const scheduleTasks = (ws, interval, count) => {
+  if (!taskSchedulerIntervalId) {
+    taskSchedulerIntervalId = setInterval(async () => {
+      const tasks = await getPendingTask(count);
+      if (tasks.length > 0) {
+        tasks.forEach((task) => {
+          const requestId = generateUniqueId();
+          const payload = {
+            action: "sendMessageToUser",
+            task: task,
+            requestId: requestId,
+          };
+          ws.send(JSON.stringify(payload));
+        });
+      } else {
+        console.log("No pending tasks");
+        clearInterval(taskSchedulerIntervalId);
+        taskSchedulerIntervalId = null;
+      }
+    }, interval * 60000);
+  }
+};
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// console.log(
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   process.env.DB_DATABASE,
-//   process.env.DB_PORT,
-//   process.env.DB_HOST
-// );
